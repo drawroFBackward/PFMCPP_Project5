6a1c18af4dd951a06a91ac30deed14d11dff8ddb
ディッドワニア アマン（Aman <C30413@muratec.co.jp>
2025-10-27 15:15:05 +0900

    copied over 3 UDTs from Proj 3. added the skeletons of 2 other UDTs. WIP

--- main.cpp
+++ main.cpp
@@ -74,25 +74,260 @@ write 3 UDTs below that EACH have:
 /*
  copied UDT 1:
  */
+struct CoffeeMaker
+{
+    CoffeeMaker();
+    int water = 100;
+    int coffeeBeanX = 10;
+    int coffeeBeanY = 10;
+    int coffeeAmount = 0;
+    std::string interface = "Touchscreen";
+    int timer = 10;
+    void makeCoffee(int amountOfWater, int amountOfCoffeeBeanX, int amountOfCoffeeBeanY);
+    void receiveCoffeeRequest(int amountOfWater, int amountOfCoffeeBeanX, int amountOfCoffeeBeanY);
+    void requestForRefill();
+    void makeDefaultCoffee();
+};
 
+CoffeeMaker::CoffeeMaker()
+{
+    std::cout << "CoffeeMaker being constructed!" << std::endl;
+}
+
+void CoffeeMaker::makeCoffee(int amountOfWater, int amountOfCoffeeBeanX, int amountOfCoffeeBeanY)
+{
+    coffeeAmount += ((amountOfWater / 10) + (amountOfCoffeeBeanX + amountOfCoffeeBeanY) / 20) * timer;
+}
+
+void CoffeeMaker::receiveCoffeeRequest(int amountOfWater, int amountOfCoffeeBeanX, int amountOfCoffeeBeanY)
+{
+    if (amountOfWater > 0 && amountOfCoffeeBeanX > 0 && amountOfCoffeeBeanY > 0)
+    {
+        std::cout << "Coffee request received." << std::endl;
+        makeCoffee(amountOfWater, amountOfCoffeeBeanX, amountOfCoffeeBeanY);
+    }
+    else
+    {
+        requestForRefill();
+    }
+}
+
+void CoffeeMaker::requestForRefill()
+{
+    std::cout << "Refill ingredients." << std::endl;
+}
+
+void CoffeeMaker::makeDefaultCoffee()
+{
+    for (int i = 0; i < timer; ++i)
+    {
+        std::cout << "Making coffee" << std::endl;
+        std::cout << "time left till coffee is ready = " << timer - i << std::endl;
+        coffeeAmount += 3;
+        --water;
+        --coffeeBeanY;
+        --coffeeBeanX;
+        std::cout << "current coffee amount = " << coffeeAmount << std::endl;
+
+        if (water == 0 || coffeeBeanX == 0 || coffeeBeanY == 0)
+        {
+            if (i == timer)
+            {
+                std::cout << "finished making coffee" << std::endl;
+            }
+            std::cout << "out of ingredients" << std::endl;
+            return;
+        }
+        std::cout << "finished making coffee" << std::endl;
+    }
+}
 /*
  copied UDT 2:
  */
+struct FireAlarmSystem
+{
+    FireAlarmSystem();
+    std::string smokeDetector = "Siemens";
+    std::string speaker = "There's a fire";
+    int serialNumber = 1;
+    std::string camera = "Canon";
+    double smokeLevel;
+    bool detectFire();
+    void soundAlarm(std::string announcement);
+    void alertFireDepartment(int phoneLine);
+    void putOutFire();
+};
 
+FireAlarmSystem::FireAlarmSystem() : smokeLevel(20.0)
+{
+    std::cout << "FireAlarmSystem being constructed!" << std::endl;
+}
+
+bool FireAlarmSystem::detectFire()
+{
+    return smokeDetector == "Siemens" && smokeLevel > 50.0;
+}
+
+void FireAlarmSystem::soundAlarm(std::string announcement)
+{
+    if (detectFire())
+    {
+        std::cout << announcement << std::endl;
+    }
+}
+
+void FireAlarmSystem::alertFireDepartment(int phoneLine)
+{
+    if (detectFire() && phoneLine > 0 && camera == "Canon")
+    {
+        std::cout << "Alerting fire department." << std::endl;
+        // pretend to send camera footage and call fire department eg. callFireDept(phoneLine); sendFootage(camera, phoneLine);
+    }
+}
+
+void FireAlarmSystem::putOutFire()
+{
+    while (detectFire())
+    {
+        smokeLevel -= 10.0;
+        std::cout << "Putting out fire, current smoke level: " << smokeLevel << std::endl;
+        if (smokeLevel <= 0.0)
+        {
+            std::cout << "Fire put out!" << std::endl;
+            smokeLevel = 0.0;
+            return;
+        }
+    }
+}
 /*
  copied UDT 3:
  */
+struct Keyboard
+{
+    Keyboard();
+    int numberOfKeys = 88;
+    int volume = 100;
+    std::string mode = "Acoustic";
+    double lcdScreen = 10.0;
+    bool pedal = false;
+    struct Key
+    {
+        Key();
+        char name;
+        bool isPressed;
+        float frequency;
+        bool isBlackKey;
+        float tuning;
+        void playKey(int time);
+        void stopKey();
+        void tuneKey(float newTuning);
+        void playTremolo();
+    };
+    void playSound(Key key);
+    void changeMode(std::string newMode);
+    void displayMode();
+    void playMelody();
 
+    Key key_1;
+};
+
+Keyboard::Keyboard()
+{
+    std::cout << "Keyboard being constructed!" << std::endl;
+}
+
+Keyboard::Key::Key() : name('C'), isPressed(false), frequency(261.63f), isBlackKey(false), tuning(0.0f)
+{
+    std::cout << "Key being constructed!" << std::endl;
+}
+
+void Keyboard::Key::playKey(int time)
+{
+    if (!isPressed)
+    {
+        isPressed = true;
+        std::cout << "Playing key: " << name << " with frequency: " << frequency + tuning << " Hz for" << time << "seconds" << std::endl;
+    }
+}
+
+void Keyboard::Key::stopKey()
+{
+    if (isPressed)
+    {
+        std::cout << "Stopping key: " << name << std::endl;
+        isPressed = false;
+    }
+}
+
+void Keyboard::Key::tuneKey(float newTuning)
+{
+    tuning += newTuning;
+}
+
+void Keyboard::Key::playTremolo()
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        playKey(1);
+        stopKey();
+    }
+}//had no clue this was what it was called XD
+
+void Keyboard::playSound(Key key)
+{
+    if (mode == "Acoustic" && pedal)
+    {
+        key.playKey(5);
+    }
+    else if (mode == "Acoustic" && !pedal)
+    {
+        key.playKey(2);
+        key.stopKey();
+    }
+}
+
+void Keyboard::changeMode(std::string newMode)
+{
+    mode = newMode;
+}
+
+void Keyboard::displayMode()
+{
+    std::cout << "Current mode: " << mode << " displayed on LCD screen size: " << lcdScreen << " inches" << std::endl;
+}
+
+void Keyboard::playMelody()
+{
+    std::string melody = "CDEFGABC";
+    for (std::string::size_type i = 0; i < melody.size(); ++i)
+    {
+        std::cout << "Playing note: " << melody[i] << std::endl;
+    }
+}
 /*
  new UDT 4:
  with 2 member functions
  */
-
+struct Kitchen
+{
+    Kitchen();
+    CoffeeMaker coffeeMaker;
+    FireAlarmSystem fireAlarmSystem;
+    void startMorningRoutine();
+	void emergencyProtocol();
+};
 /*
  new UDT 5:
  with 2 member functions
  */
-
+struct House
+{
+    House();
+    Keyboard keyboard;
+    Kitchen kitchen;
+    void relax();
+	void dailyChores();
+};
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
